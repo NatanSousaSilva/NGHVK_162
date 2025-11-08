81e65ec25999b99f385b8fd784669b2cfff23d97
Natan Sousa <natanssousassiva@gmail.com>
2025-11-08 18:50:19 -0300

    Remove pastas temporárias build, install e log do repositório

--- build/listening_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/natan/Documents/NGHVK/ros2_ws/src/listening_control
-# Build directory: /home/natan/Documents/NGHVK/ros2_ws/build/listening_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
